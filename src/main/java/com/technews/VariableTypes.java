100bb429370612d1e98b1dd7c00a3f2e79156a93
marcuspramirez <@macusramirez@gmail.com>
2021-09-08 09:23:09 -0500

    foundation

---
+++ src/main/java/com/technews/VariableTypes.java
@@ -0,0 +1,16 @@
+package com.technews;
+
+public class VariableTypes {
+    public static void main(String[] args) {
+        int a = 4;
+        double b = 3.5;
+        String c = "cat";
+
+        double sum = a + b;
+
+        String concat = a + c;
+
+        System.out.println(concat);
+
+    }
+}
